43b67b408445809cf4de3b8a664136b1836a671c
GabContreras <contrerasgab01@gmail.com>
2025-03-03 11:23:38 -0600

    jaja xd

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+//funciones de la app, tipo login, crud, registros, etc
+
+
+//inserto todo lo de la libreria express
+import express from "express";
+// Creo una constante que es igual a la libreria que acabo de importar, y la ejecuto 
+
+const app = express()
+//Exporti esta constante para usar express en todos lados
+export default app;
